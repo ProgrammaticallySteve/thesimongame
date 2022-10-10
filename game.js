cba1e59038528609c2581182cb1b7726451b6ccb
Stephen Harvey <itsteveharvey@gmail.com>
2022-10-10 14:46:07 +1300

    Created new playSound function for when a user clicks on a button the corresponding sound should be played. Also refactored the code in nextSequence function

--- game.js
+++ game.js
@@ -8,8 +8,9 @@ var userClickedPattern = [];
 $(".btn").click(function() {
 
 var userChosenColour = $(this).attr("id");
-
 userClickedPattern.push(userChosenColour);
+
+playSound(userChosenColour);
 
 });
 
@@ -23,7 +24,11 @@ function nextSequence () {
 
 $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-var audio = new Audio ("sounds/" + randomChosenColour + ".mp3");
-audio.play();
+playSound(userChosenColour);
 
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }

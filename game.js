fcca08a0fbc557f89135b064745eda159a7e4ec7
Stephen Harvey <itsteveharvey@gmail.com>
2022-10-10 14:15:47 +1300

    Created buttonColours array, created function to choose randomNumber and randomChosenColour

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-alert ("Hello World");
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence () {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+
+}
